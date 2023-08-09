c188d6d0c99950e901b1459ff5fce25754cf42a9
Luke Smeenge <Luke170607@gmail.com>
2023-08-08 22:15:47 -0700

    Sprint 6 added validation.js and modal popups initial commit

---
+++ scripts/validation.js
@@ -0,0 +1,87 @@
+// Error message //
+
+function showError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.validationMessage;
+  errorMessageEl.classList.add(errorClass);
+}
+
+function hideError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(errorClass);
+}
+
+// Validity checkers and input //
+
+function checkInputValidity(formEl, inputEl, content) {
+  if (!inputEl.validity.valid) {
+    return showError(formEl, inputEl, content);
+  }
+
+  hideError(formEl, inputEl, content);
+}
+ 
+function hasInvalidInput(inputList) {
+    return !inputList.every((inputEl) => inputEl.validity.valid); 
+}
+
+// Button state //
+
+const disableButton = (submitButton, { inactiveButtonClass }) => {
+  submitButton.classList.add(inactiveButtonClass);
+  submitButton.disabled = true;
+};
+
+const enableButton = (submitButton, { inactiveButtonClass }) => {
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
+};
+
+const toggleButtonState = (inputEls, submitButton, { inactiveButtonClass }) => {
+  if (hasInvalidInput(inputEls)) {
+    disableButton(submitButton, { inactiveButtonClass });
+    return;
+  }
+
+  enableButton(submitButton, { inactiveButtonClass });
+};
+
+// Listeners and validation //
+
+function setEventListeners(formEl, content) {
+    const {inputSelector}= content;
+    const inputEls = [...formEl.querySelectorAll(inputSelector)];
+    const submitButton = formEl.querySelector(content.submitButtonSelector);
+    inputEls.forEach((inputEl) => {
+      inputEl.addEventListener("input", (e) => {
+        checkInputValidity(formEl, inputEl, content);
+        toggleButtonState(inputEls, submitButton, content);
+      });
+    });
+
+    toggleButtonState(inputEls, submitButton, content);
+  }
+   
+  function enableValidation(content) {
+    const formEls = [...document.querySelectorAll(content.formSelector)];
+    formEls.forEach((formEl) => {
+      formEl.addEventListener('submit,' (e) => {
+        e.preventDefault(),
+      });
+      setEventListeners(formEl, content);
+    });
+  }
+
+const config = {
+    formSelector: ".modal__form",
+    inputSelector: ".modal__input",
+    submitButtonSelector: ".modal__button",
+    inactiveButtonClass: "modal__button_disabled",
+    inputErrorClass: "modal__input_type_error",
+    errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
