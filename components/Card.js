0626a0a9fe4a1d0f0b915a3f6e930e0239c3e338
Luke Smeenge <Luke170607@gmail.com>
2023-08-29 12:34:53 -0700

    moved cards rendering into getView() card.js Sprint 7

--- components/Card.js
+++ components/Card.js
@@ -8,52 +8,48 @@ export default class Card {
   constructor({ name, link }, cardTemplateSelector) {
     this._Name = name;
     this._Link = link;
-    this._CardTemplateSelector = cardTemplateSelector;
+    this._cardTemplateSelector = cardTemplateSelector;
 
-    this._Element = document
-      .querySelector(this._CardTemplateSelector)
-      .content.querySelector(".card")
-      .cloneNode(true);
-    this._LikeButton = this._Element.querySelector(".card__like-button");
-    this._DeleteButton = this._Element.querySelector(".card__delete-button");
-    this._CardImage = this._Element.querySelector(".card__image");
-    this._CardTitle = this._Element.querySelector(".card__title");
+    this._likeButton = this._element.querySelector(".card__like-button");
+    this._deleteButton = this._element.querySelector(".card__delete-button");
+    this._cardImage = this._element.querySelector(".card__image");
+    this._cardTitle = this._element.querySelector(".card__title");
   }
 
   _handleLikeIcon() {
-    this._LikeButton.classList.toggle("card__like-button_active");
+    this._likeButton.classList.toggle("card__like-button_active");
   }
 
   _handleDeleteCard() {
-    this._Element.remove();
+    this._element.remove();
   }
 
   _handlePreviewPicture() {
     previewPhoto.src = this._Link;
-    previewPhoto.alt = this._Name;
-    previewTitle.innerText = this._Name;
+    previewPhoto.alt = this._name;
+    previewTitle.innerText = this._name;
     openPopup(fullPhotoPopup);
   }
 
   _handlePhotoPopup() {
     previewPhoto.src = this._Link;
-    previewPhoto.alt = this._Name;
-    previewTitle.innerText = this._Name;
+    previewPhoto.alt = this._name;
+    previewTitle.innerText = this._name;
     openPopup(fullPhotoPopup);
   }
 
   _setEventListeners() {
     //".card__like-button_active"
-    this._LikeButton.addEventListener("click", () => {
+    this._likeButton.addEventListener("click", () => {
       this._handleLikeIcon();
     });
 
     //".card__delete-button"
-    this._DeleteButton.addEventListener("click", () => {
+    this._deleteButton.addEventListener("click", () => {
       this._handleDeleteCard();
     });
 
-    this._CardImage.addEventListener("click", () => {
+    this._cardImage.addEventListener("click", () => {
       this._handlePhotoPopup();
     });
   }
@@ -61,10 +57,19 @@ export default class Card {
   getView() {
     this._setEventListeners();
 
-    this._CardImage.src = this._Link;
-    this._CardImage.alt = this._Name;
-    this._CardTitle.textContent = this._Name;
+    const element = document
+      .querySelector(this._cardTemplateSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
 
-    return this._Element;
+    // instead of the filling random variables below, you need to fill with the info an element
+    this._cardImage.src = this._link;
+    this._cardImage.alt = this._name;
+    this._cardTitle.textContent = this._name;
+
+    return element;
   }
 }
+
+// camelCase
+// PascalCase
