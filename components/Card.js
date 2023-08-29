97eef2879fac60c38ba5ac6f7065712489bec43d
Luke Smeenge <Luke170607@gmail.com>
2023-08-29 11:07:14 -0700

    card.js updates Sprint 7

--- components/Card.js
+++ components/Card.js
@@ -21,11 +21,11 @@ export default class Card {
   }
 
   _handleLikeIcon() {
-    this._likeButton.classList.toggle("card__like-button_active");
+    this._LikeButton.classList.toggle("card__like-button_active");
   }
 
   _handleDeleteCard() {
-    this._element.remove();
+    this._Element.remove();
   }
 
   _handlePreviewPicture() {
@@ -44,7 +44,7 @@ export default class Card {
 
   _setEventListeners() {
     //".card__like-button_active"
-    this._likeButton.addEventListener("click", () => {
+    this._LikeButton.addEventListener("click", () => {
       this._handleLikeIcon();
     });
 
